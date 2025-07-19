c9f01ed3046341367760faf1c3906d6141a27991
MiaowuAbobo <2496330469@qq.com>
2025-07-19 23:23:36 +0800

    Add files via upload

---
+++ diansai2024Hsilun/Debug/Hardware/subdir_vars.mk
@@ -0,0 +1,62 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Hardware/atk_ms901m.c \
+../Hardware/atk_ms901m_uart.c \
+../Hardware/delay.c \
+../Hardware/gpio_zzk.c \
+../Hardware/huidu5.c \
+../Hardware/motor.c \
+../Hardware/oled.c 
+
+C_DEPS += \
+./Hardware/atk_ms901m.d \
+./Hardware/atk_ms901m_uart.d \
+./Hardware/delay.d \
+./Hardware/gpio_zzk.d \
+./Hardware/huidu5.d \
+./Hardware/motor.d \
+./Hardware/oled.d 
+
+OBJS += \
+./Hardware/atk_ms901m.o \
+./Hardware/atk_ms901m_uart.o \
+./Hardware/delay.o \
+./Hardware/gpio_zzk.o \
+./Hardware/huidu5.o \
+./Hardware/motor.o \
+./Hardware/oled.o 
+
+OBJS__QUOTED += \
+"Hardware\atk_ms901m.o" \
+"Hardware\atk_ms901m_uart.o" \
+"Hardware\delay.o" \
+"Hardware\gpio_zzk.o" \
+"Hardware\huidu5.o" \
+"Hardware\motor.o" \
+"Hardware\oled.o" 
+
+C_DEPS__QUOTED += \
+"Hardware\atk_ms901m.d" \
+"Hardware\atk_ms901m_uart.d" \
+"Hardware\delay.d" \
+"Hardware\gpio_zzk.d" \
+"Hardware\huidu5.d" \
+"Hardware\motor.d" \
+"Hardware\oled.d" 
+
+C_SRCS__QUOTED += \
+"../Hardware/atk_ms901m.c" \
+"../Hardware/atk_ms901m_uart.c" \
+"../Hardware/delay.c" \
+"../Hardware/gpio_zzk.c" \
+"../Hardware/huidu5.c" \
+"../Hardware/motor.c" \
+"../Hardware/oled.c" 
+
+
---
+++ diansai2024Hsilun/Debug/subdir_vars.mk
@@ -0,0 +1,70 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+SYSCFG_SRCS += \
+../diansai2024H.syscfg 
+
+C_SRCS += \
+../diansai2024H.c \
+./ti_msp_dl_config.c \
+E:/ti/mspm0_sdk_2_01_00_03/source/ti/devices/msp/m0p/startup_system_files/ticlang/startup_mspm0g350x_ticlang.c 
+
+GEN_CMDS += \
+./device_linker.cmd 
+
+GEN_FILES += \
+./device_linker.cmd \
+./device.opt \
+./ti_msp_dl_config.c 
+
+C_DEPS += \
+./diansai2024H.d \
+./ti_msp_dl_config.d \
+./startup_mspm0g350x_ticlang.d 
+
+GEN_OPTS += \
+./device.opt 
+
+OBJS += \
+./diansai2024H.o \
+./ti_msp_dl_config.o \
+./startup_mspm0g350x_ticlang.o 
+
+GEN_MISC_FILES += \
+./device.cmd.genlibs \
+./ti_msp_dl_config.h \
+./Event.dot 
+
+OBJS__QUOTED += \
+"diansai2024H.o" \
+"ti_msp_dl_config.o" \
+"startup_mspm0g350x_ticlang.o" 
+
+GEN_MISC_FILES__QUOTED += \
+"device.cmd.genlibs" \
+"ti_msp_dl_config.h" \
+"Event.dot" 
+
+C_DEPS__QUOTED += \
+"diansai2024H.d" \
+"ti_msp_dl_config.d" \
+"startup_mspm0g350x_ticlang.d" 
+
+GEN_FILES__QUOTED += \
+"device_linker.cmd" \
+"device.opt" \
+"ti_msp_dl_config.c" 
+
+C_SRCS__QUOTED += \
+"../diansai2024H.c" \
+"./ti_msp_dl_config.c" \
+"E:/ti/mspm0_sdk_2_01_00_03/source/ti/devices/msp/m0p/startup_system_files/ticlang/startup_mspm0g350x_ticlang.c" 
+
+SYSCFG_SRCS__QUOTED += \
+"../diansai2024H.syscfg" 
+
+
